af8d2469cbeb0b1afd89a356ae5cdf1b0d8d0014
chihiro hashimoto <chr@chrhsmt.com>
2017-10-09 14:40:47 +0900

    Create parties table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008021837) do
+ActiveRecord::Schema.define(version: 20171008040746) do
+
+  create_table "parties", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "short_name", null: false
+    t.string "full_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "prefs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "pref_code", null: false
