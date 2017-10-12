f6542a3a0d75ec26b3d24be1c344a0250103d6e7
Yuya Tanaka <yuya.presto@gmail.com>
2017-10-13 02:13:33 +0900

    Add search by candidate name feature

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -15,11 +15,28 @@ class SearchController < ApplicationController
     @title = "#{@senkyoku.name}の候補者一覧"
   end
 
+  def by_name
+    @keyword = params[:keyword]
+    tokens = @keyword.scan(/\p{hiragana}+|\p{katakana}+|\p{Han}+|[A-Za-z]+/)
+    last_like = like_escape(tokens.first || '') + '%'
+    first_like = like_escape((tokens[1..-1] || []).join('')) + '%'
+
+    @candidates = Candidate.where(
+      '(name_last LIKE ? OR name_last_furigana LIKE ?)' +
+      ' AND (name_first LIKE ? OR name_first_furigana LIKE ?)',
+      last_like, last_like, first_like, first_like
+    ).order('submission_order ASC').limit(100).to_a
+  end
 
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def search_params
       params.permit(:zip_code)
+    end
+
+    def like_escape(str)
+      # http://d.hatena.ne.jp/teracc/20090703/1246576620
+      str.gsub(/([\\%_])/, '\\\\\\1')
     end
 
 end
