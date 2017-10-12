78116f3381e80b92e8824b62169873dbb8c15d3b
ikt2y <ikd618@gmail.com>
2017-10-12 23:39:50 +0900

    Add description to component index path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: "search#new"
-  get "/index", to: "search#index"
   get "/search", to: "search#show"
   get '/senkyoku/:pref_code', to: 'prefs#show'
   get '/senkyoku/:pref_code/:senkyoku_no', to: 'senkyokus#show'
@@ -8,8 +7,8 @@ Rails.application.routes.draw do
   resources :candidates, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
-
+  # components index page
+  get "/index", to: "search#index"
 
   get '*path', controller: 'application', action: 'render_404'
 end
