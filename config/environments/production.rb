3d25cd400b675787744bbb504215c7c4f929d654
Hal Seki <hal@plants-web.jp>
2018-04-02 20:56:23 +0900

    add log rotation setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  config.logger = Logger.new("log/production.log", 5, 1 * 1024 * 1024)
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
